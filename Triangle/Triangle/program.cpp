f336d299dcb815211663bc23a9c1057d6dd12726
Gábor Tóth <tthgbr2017@gmail.com>
2022-09-20 22:45:30 +0200

    Used the concept of element buffer objects(EBO) to draw a rectangle.

--- Triangle/Triangle/program.cpp
+++ Triangle/Triangle/program.cpp
@@ -99,18 +99,30 @@ int main()
 	// vertex data
 	GLfloat vertices[] =
 	{
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+
+		  0.5f,  0.5f,  0.0f, // top right
+		  0.5f, -0.5f,  0.0f, // bottom right
+		 -0.5f, -0.5f,  0.0f, // bottom left
+		 -0.5f,  0.5f,  0.0f  // top left 
 	};
 
-	GLuint VBO = 0, VAO = 0;
+	GLuint indices[] =
+	{
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	GLuint VBO = 0, EBO = 0, VAO = 0;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 	
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
@@ -126,7 +138,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// check and call events and swap the buffers
 		glfwPollEvents();
